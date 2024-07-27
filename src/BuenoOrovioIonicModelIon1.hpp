274b4cb5b3f638f712a6337dbd301651c4f8df16
Sabrina Cesaroni <sabrina.cesaroni@mail.polimi.it>
2024-07-27 09:05:39 +0200

    little correction

--- src/BuenoOrovioIonicModelIon1.hpp
+++ src/BuenoOrovioIonicModelIon1.hpp
@@ -6,10 +6,10 @@ class BuenoOrovioIonicModelIon1 : public BuenoOrovioIonicModel<K> {
 public:
     //u(i) = u_k-i
     double implicit_coefficient(const std::array<double, K_ion>& u, size_t valid_u, const GatingVariables<N>& vars) override {
-        return -vars.get(0)*H(u[0] - theta_v)*(u_u - u[0]) + (1 - H(u[0] - theta_v))/tau_o(u[0]);
+        return -(vars.get(0)*H(u[0] - theta_v)*(u_u - u[0]))/tau_fi + (1 - H(u[0] - theta_w))/tau_o(u[0]);
     }
     double explicit_coefficient(const std::array<double, K_ion>& u, size_t valid_u, const GatingVariables<N>& vars) override {
-        return -theta_v * v * H(u[0] - theta_v) * (u_u - u[0]) - u_0 / tau_o(u[0]) * (1 - H(u[0] - theta_w)) + 1/tau_so(u[0]) * H(u[0] - theta_w) - H(u[0] - theta_w)*vars.get(1)*vars.get(2)/tau_si;
+        return -(theta_v * vars.get(0) * H(u[0] - theta_v) * (u_u - u[0]))/tau_fi - u_0 / tau_o(u[0]) * (1 - H(u[0] - theta_w)) + 1/tau_so(u[0]) * H(u[0] - theta_w) - H(u[0] - theta_w)*vars.get(1)*vars.get(2)/tau_si;
     }
 
 };
