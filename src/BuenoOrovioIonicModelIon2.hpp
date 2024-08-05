0359ae2444df585d6aaa8d9325107ba4d4bb1d71
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-08-05 10:17:22 +0200

    Added second ionic model

---
+++ src/BuenoOrovioIonicModelIon2.hpp
@@ -0,0 +1,24 @@
+#ifndef BUENO_OROVIO_IONIC_MODEL_ION2_H
+#define BUENO_OROVIO_IONIC_MODEL_ION2_H
+#include "BuenoOrovioIonicModel.hpp"
+#include "utils.hpp"
+
+template<int K_ion, int N_ion>
+class BuenoOrovioIonicModelIon2 : public BuenoOrovioIonicModel<K_ion, N_ion> {
+public:
+    //u(i) = u_k-i
+    double implicit_coefficient(const std::array<double, K_ion>& u, size_t /*valid_u*/, GatingVariables<N_ion>& vars) override {
+        return (1 - this->H(u[0] - this->theta_w))/this->tau_o(u[0])  + vars.get(0) / this->tau_fi * this->H(u[0] - this->theta_v) * (u[0] - this->theta_v);
+    }
+
+    double explicit_coefficient(const std::array<double, K_ion>& u, size_t /*valid_u*/, GatingVariables<N_ion>& vars) override {
+        return -this->u_u * vars.get(0) / this->tau_fi  * this->H(u[0] - this->theta_v) * (u[0] - this->theta_v) - 
+        this->u_0 * (1 - this->H(u[0] - this->theta_w))/this->tau_o(u[0]) + this->H(u[0] - this->theta_w) / this->tau_so(u[0]) -
+         this->H(u[0] - this->theta_w) * vars.get(1) * vars.get(2) / this->tau_si;
+    }
+
+    ~BuenoOrovioIonicModelIon2() override{}
+
+};
+
+#endif
