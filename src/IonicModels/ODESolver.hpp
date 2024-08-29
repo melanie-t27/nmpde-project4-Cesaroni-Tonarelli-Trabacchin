88e0aee6a6323ce3907a3cdb792cac400bebb64d
Tommaso Trabacchin <tommaso.trabacchin@mail.polimi.it>
2024-08-30 00:11:30 +0200

    code refactor

--- src/IonicModels/ODESolver.hpp
+++ src/IonicModels/ODESolver.hpp
@@ -5,16 +5,19 @@
 #include "../utils.hpp"
 #include "../VectorView.hpp"
 
+// class responsible for solving the system of ODEs
 template<int N_ion>
 class ODESolver {
 public:
 
+    // Constructor. We provide theta method parameter and the ionic model.
     ODESolver(double theta_, double deltat_, std::shared_ptr<IonicModel<N_ion>> ionic_model_) :
             theta(theta_),
             deltat(deltat_),
             ionic_model(ionic_model_)
     {}
 
+    // the method takes in input a view of the FE Solution and a view of the gating variables
     template<typename VectorType>
     void solve(VectorView<VectorType>& u, std::vector<VectorView<VectorType>>& vars) {
         for(size_t i = 0; i < u.size(); i++) {
